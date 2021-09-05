eed74e1432283b9904a6121c715b012a3a575e82
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-09-05 18:01:07 +0900

    au packet

--- PacketTunnel/PacketTunnelProvider.swift
+++ PacketTunnel/PacketTunnelProvider.swift
@@ -6,34 +6,34 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
     private var udpSessions = [String: UDPSession]()
 
     override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
-            let ip = "10.10.10.10"
-            let subnetMask = "255.255.255.0"
+        let ip = "10.10.10.10"
+        let subnetMask = "255.255.255.0"
 
-            let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "127.0.0.1")
-            let ipv4Settings = NEIPv4Settings(addresses: [ip], subnetMasks: [subnetMask])
+        let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "127.0.0.1")
+        let ipv4Settings = NEIPv4Settings(addresses: [ip], subnetMasks: [subnetMask])
 
-            ipv4Settings.includedRoutes = [NEIPv4Route.default()]
-            ipv4Settings.excludedRoutes = [
-                NEIPv4Route(destinationAddress: "10.0.0.0", subnetMask: "255.0.0.0"),
-                NEIPv4Route(destinationAddress: "127.0.0.0", subnetMask: "255.0.0.0"),
-                NEIPv4Route(destinationAddress: "192.168.0.0", subnetMask: "255.255.0.0"),
-            ]
-            settings.ipv4Settings = ipv4Settings
+        ipv4Settings.includedRoutes = [NEIPv4Route.default()]
+        ipv4Settings.excludedRoutes = [
+            NEIPv4Route(destinationAddress: "10.0.0.0", subnetMask: "255.0.0.0"),
+            NEIPv4Route(destinationAddress: "127.0.0.0", subnetMask: "255.0.0.0"),
+            NEIPv4Route(destinationAddress: "192.168.0.0", subnetMask: "255.255.0.0"),
+        ]
+        settings.ipv4Settings = ipv4Settings
 
-            let dnsSettings = NEDNSSettings(servers: ["8.8.8.8", "8.8.4.4"])
-            dnsSettings.matchDomains = [""]
-            settings.dnsSettings = dnsSettings
+        let dnsSettings = NEDNSSettings(servers: ["8.8.8.8", "8.8.4.4"])
+        dnsSettings.matchDomains = [""]
+        settings.dnsSettings = dnsSettings
 
-            setTunnelNetworkSettings(settings) { [weak self] error in
-                guard let self = self else { return }
+        setTunnelNetworkSettings(settings) { [weak self] error in
+            guard let self = self else { return }
 
-                completionHandler(error)
-                if let error = error {
-                    NSLog("Tunnel network settings error: \(error)")
-                } else {
-                    self.localPacketsToServer()
-                }
+            completionHandler(error)
+            if let error = error {
+                NSLog("Tunnel network settings error: \(error)")
+            } else {
+                self.localPacketsToServer()
             }
+        }
     }
     
     override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
@@ -41,12 +41,12 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
     }
 
     private func localPacketsToServer() {
-        packetFlow.readPackets { [weak self] (packets, _) in
+        packetFlow.readPacketObjects { [weak self] (packets) in
             defer { self?.localPacketsToServer() }
             guard let self = self else { return }
 
-            packets.forEach { (data) in
-                if let ipPacket = IPPacket(data) {
+            packets.forEach { (packet) in
+                if let ipPacket = IPPacket(packet.data) {
                     switch ipPacket.proto {
                     case .tcp:
                         self.sendTCPPacket(ipPacket)
@@ -70,7 +70,16 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
 
     private func sendUDPPacket(_ packet: IPPacket) {
         let key = "\(packet.source):\(packet.sourcePort) => \(packet.destination):\(packet.destinationPort)"
-        NSLog("SEND: \(key) \(packet.proto) \(packet.payload.hex)")
+        NSLog("SEND: \(key) \(packet.payload.hex)")
+
+        let port = packet.destinationPort
+        if port == 22023 || port == 22123 || port == 22223 || port == 22323 ||
+            port == 22423 || port == 22523 || port == 22623 || port == 22723 ||
+            port == 22823 || port == 22923 {
+            if packet.payload.hex.hasPrefix("01") {
+                NSLog("handleEvent: => \(packet.payload.hex)")
+            }
+        }
 
         if let session = self.udpSessions[key] {
             session.send(packet.payload)
@@ -92,88 +101,10 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                     payload: data
                 )
 
-//                NSLog("RECV: \(packet.source):\(packet.sourcePort) => \(packet.destination):\(packet.destinationPort) \(packet.packetData.hex)")
+                NSLog("RECV: \(packet.source):\(packet.sourcePort) => \(packet.destination):\(packet.destinationPort) \(data.hex)")
 
                 if let auPacket = PacketParser.parse(packet: data) {
-//                    NSLog("AUPK: \(auPacket)")
-                    switch auPacket {
-                    case .normal(let nomal):
-                        break
-                    case .reliable(let reliable):
-                        NSLog("AUPK: RECV: \(auPacket) \(data.hex)")
-                        for message in reliable.messages {
-                            switch message.payload {
-                            case .hostGame(let hostGame):
-                                NSLog("AUPK: hostGame: \(hostGame)")
-                            case .joinGame(let joinGame):
-                                NSLog("AUPK: joinGame: \(joinGame)")
-                            case .startGame(let startGame):
-                                NSLog("AUPK: startGame: \(startGame)")
-                            case .gameData(let gameData):
-                                for message in gameData.messages {
-                                    switch message.payload {
-                                    case .data(let data):
-                                        break
-                                    case .rpc(let rpc):
-                                        switch rpc.payload {
-                                        case .syncSettings(_):
-                                            break
-                                        case .setInfected(let setInfected):
-                                            NSLog("AUPK: setInfected: \(setInfected)")
-                                        case .setName(let setName):
-                                            break
-                                        case .setColor(let setColor):
-                                            break
-                                        case .setHat(let setHat):
-                                            break
-                                        case .setSkin(_):
-                                            break
-                                        case .murderPlayer(let murderPlayer):
-                                            NSLog("AUPK: murderPlayer: [\(rpc.senderNetId)] \(rpc.rpcCallId) killed \(murderPlayer)")
-                                        case .startMeeting(_):
-                                            break
-                                        case .sendChatNote(_):
-                                            break
-                                        case .setPet(_):
-                                            break
-                                        case .setStartCounter(_):
-                                            break
-                                        case .close:
-                                            break
-                                        case .votingComplete(_):
-                                            break
-                                        case .castVote(_):
-                                            break
-                                        case .setTasks(_):
-                                            break
-                                        case .updateGameData(_):
-                                            break
-                                        }
-                                    case .spawn(let spawn):
-                                        break
-                                    case .despawn(let despawn):
-                                        break
-                                    }
-                                }
-                            case .endGame(let endGame):
-                                NSLog("AUPK: endGame: \(endGame)")
-                            case .redirect(let redirect):
-                                NSLog("AUPK: redirect: \(redirect)")
-                            case .reselectServer(let reselectServer):
-                                NSLog("AUPK: reselectServer: \(reselectServer)")
-                            }
-                        }
-                    case .hello(let hello):
-                        break
-                    case .disconnect:
-                        NSLog("AUPK: disconnect")
-                    case .acknowledgement(let acknowledgement):
-                        break
-                    case .fragment:
-                        break
-                    case .ping(let ping):
-                        break
-                    }
+                    handleEvent(packet: auPacket, data: data)
                 }
 
                 self.packetFlow.writePacketObjects([
@@ -188,6 +119,146 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
             }
 
             self.udpSessions[key] = session
+        }
+    }
+}
+
+private func handleEvent(packet: Packet, data: Data) {
+    switch packet {
+    case .reliable(let reliable):
+        NSLog("handleEvent: \(packet) \(data.hex)")
+        for message in reliable.messages {
+            switch message.payload {
+            case .hostGame(let hostGame):
+                NSLog("Host Game: \(hostGame)")
+            case .joinGame(let joinGame):
+                NSLog("Join Game: \(joinGame.gameCode)")
+            case .startGame:
+                NSLog("Start Game")
+            case .gameData(let gameData):
+                handleGameData(gameData: gameData)
+            case .endGame(let endGame):
+                NSLog("End Game: \(endGame)")
+            case .redirect(_):
+                break
+            case .reselectServer(_):
+                break
+            }
+        }
+        break
+    case .disconnect:
+        break
+    case .normal, .hello, .acknowledgement, .fragment, .ping:
+        break
+    }
+}
+
+private func handleGameData(gameData: GameData) {
+    var despawning = false
+
+    for message in gameData.messages {
+        switch message.payload {
+        case .data:
+            break
+        case .rpc(let rpc):
+            switch rpc.payload {
+            case .syncSettings:
+                break
+            case .setInfected(let infected):
+//                var playerInfos = [ImmutablePlayer]()
+//                for player in GameStateCapture.shared.players {
+//                    playerInfos.append(ImmutablePlayer(name: player.name, isImpostor: infected.impostors.contains(player.id)))
+//                }
+//                GameStateCapture.shared.playerInfos = playerInfos
+                break
+            case .setName:
+                break
+            case .setColor, .setHat, .setSkin:
+                break
+            case .startMeeting:
+                break
+            case .sendChatNote, .setPet, .setStartCounter:
+                break
+            case .close:
+                break
+            case .votingComplete:
+                break
+            case .castVote, .setTasks:
+                break
+            case .updateGameData(let gameData):
+                let players = gameData.map { (playerData) in
+                    let flags = playerData.flags
+                    let isDisconnected = (flags & 1) != 0
+                    let isDead = (flags & 4) != 0
+
+                    NSLog("Player Data: \(playerData)")
+                }
+
+//                if despawning {
+//                    for player in GameStateCapture.shared.players {
+//                        if !players.contains(player) {
+//                            GameEventManager.shared.changeState(
+//                                event: PlayerChangedEvent(
+//                                    action: .left,
+//                                    name: player.name,
+//                                    isDead: player.isDead,
+//                                    isDisconnected: player.isDisconnected,
+//                                    color: player.color
+//                                )
+//                            )
+//                        }
+//                    }
+//                } else {
+//                    for player in players {
+//                        if GameStateCapture.shared.players.contains(player) {
+//                            GameEventManager.shared.changeState(
+//                                event: PlayerChangedEvent(
+//                                    action: .changedColor,
+//                                    name: player.name,
+//                                    isDead: player.isDead,
+//                                    isDisconnected: player.isDisconnected,
+//                                    color: player.color
+//                                )
+//                            )
+//                        } else {
+//                            GameEventManager.shared.changeState(
+//                                event: PlayerChangedEvent(
+//                                    action: .joined,
+//                                    name: player.name,
+//                                    isDead: player.isDead,
+//                                    isDisconnected: player.isDisconnected,
+//                                    color: player.color
+//                                )
+//                            )
+//                        }
+//                    }
+//                }
+//
+//                GameStateCapture.shared.players = players
+            case .murderPlayer(let murderPlayer):
+                NSLog("Murder Player: \(murderPlayer)")
+            }
+        case .spawn(let spawn):
+            switch spawn.spawnType {
+            case .shipStatus:
+                break
+            case .meetingHud:
+                break
+            case .lobbyBehaviour:
+                break
+            case .gameData:
+                break
+            case .playerControl:
+                break
+            case .headquarters:
+                break
+            case .planetMap:
+                break
+            case .aprilShipStatus:
+                break
+            }
+        case .despawn:
+            despawning = true
         }
     }
 }
